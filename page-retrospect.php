e75e9869445ef979660defbe12db70ba992f1d05
sphrcl <info@sphericalcommunications.com>
2014-05-09 14:20:49 -0400

    upding this shit

--- page-retrospect.php
+++ page-retrospect.php
@@ -27,14 +27,11 @@
 </div>
 
 
-
+<div class="columns clear half">
 
 <?php $loop = new WP_Query( array( 'post_type' => 'acme_retrospect', 'posts_per_page' => -1 ) ); ?>
 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-
-
-<div class="columns clear half">
 <div class="half project">
 	<div class="half project-image">
 		<?php 
@@ -42,7 +39,7 @@
                   the_post_thumbnail();
                 } 
                 ?>
-		<div class="project-type"><p>Video</p></div>		
+		<div class="project-type"><p><?php single_cat_title(''); ?></p></div>		
 	</div>
 <h2><?php the_title(); ?></h2>
 <p><?php the_excerpt(); ?><br /></p>
@@ -50,25 +47,13 @@
 	<p><a href="<?php the_permalink() ?>">↪</a></p>
 </div>	
 </div>
-<div class="half project">
-	<div class="half project-image">
-		<?php 
-                if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                  the_post_thumbnail();
-                } 
-                ?>
-		<div class="project-type"><p>Video</p></div>		
-	</div>
-<h2><?php the_title(); ?></h2>
-<p><?php the_excerpt(); ?><br /></p>
-<div class="linker">
-	<p><a href="<?php the_permalink() ?>">↪</a></p>
-</div>	
-</div>
+
+<?php endwhile; wp_reset_query(); ?>
+
 </div>
 	
 
-<?php endwhile; wp_reset_query(); ?>
+
 
 </div>
 <!-- end: content -->
