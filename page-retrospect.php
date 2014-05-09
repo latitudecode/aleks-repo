141dd4ea8df8b527ab98c6caf8398f0bf07bb9e2
sphrcl <info@sphericalcommunications.com>
2014-05-09 12:49:21 -0400

    adding titles to retrospect

--- page-retrospect.php
+++ page-retrospect.php
@@ -10,4 +10,83 @@
 
 <?php get_header(); ?>
 
+	<!-- body -->
+	<body class="blog">
+	
+
+
+<!-- content -->
+<div class="content">
+
+
+<div class="intro-text">
+<h1>Retrospect</h1>
+
+<div class="liner"></div>
+<p><?php the_content('index.php?page_id=6'); ?></p>
+</div>
+
+
+<div class="columns clear half">
+<div class="half project">
+	<div class="half project-image">
+		<img src="images/project-image.jpg" />
+		<div class="project-type"><p>Video</p></div>		
+	</div>
+<h2>Original Hooligans</h2>
+<p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.<br />
+<div class="linker">
+	<p><a href="#">↪</a></p>
+</div>	
+</div>
+<div class="half project">
+<div class="half project-image">
+	<img src="images/project-image.jpg" />
+		<div class="project-type"><p>Video</p></div>		
+
+</div>
+
+<h2>Pilgrims</h2>
+<p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.<br />
+<div class="linker">
+	<p><a href="#">↪</a></p>
+</div>	
+</div>
+</div>
+
+<div class="columns clear half">
+<div class="half project">
+<div class="half project-image">
+	<img src="images/project-image.jpg" />
+		<div class="project-type"><p>Video</p></div>		
+
+</div>
+<h2>Original Hooligans</h2>
+<p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.<br />
+<div class="linker">
+	<p><a href="#">↪</a></p>
+</div>	
+</div>
+<div class="half project">
+<div class="half project-image">
+	<img src="images/project-image.jpg" />
+			<div class="project-type"><p>Video</p></div>		
+</div>
+
+<h2>Pilgrims</h2>
+<p>Nulla vitae elit libero, a pharetra augue. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.<br />
+<div class="linker">
+	<p><a href="#">↪</a></p>
+</div>	
+</div>
+</div>
+	
+	
+</div>
+<!-- end: content -->
+
+
+
+
+
 <?php get_footer(); ?>
